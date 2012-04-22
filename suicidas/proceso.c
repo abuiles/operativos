ae314e30889350cc79b74cb05ff541b7da4fafe5
Adolfo Builes <builes.adolfo@gmail.com>
2012-04-22 16:21:35 -0500

    Make proceso win/nix friendly.

--- suicidas/proceso.c
+++ suicidas/proceso.c
@@ -1,24 +1,26 @@
-#include "dbg.h"
-#include <stdlib.h>
-#include <unistd.h>
+#include <stdio.h>
+#include <windows.h>
+/* #include <unistd.h> */
 
 int main(int argc, char *argv[])
 {
 
 
-  fprintf(stdout, "Comenzando proceso hijo de  %d\n", getppid());
+  fprintf(stdout, "Comenzando proceso hijo de pending getppid()\n");
+  fflush(stdout);
   int i;
   int j;
-
   for (i = 0; i < 5; ++i){
-    fprintf(stdout, "Hi hi proceso del proceso %d\n", getppid());
-    fprintf(stdout, "Hi hi my pid is %d\n", getpid());
+    fprintf(stdout, "Hi hi proceso del proceso pending getppid\n  ");
+    fprintf(stdout, "Hi hi my pid is pending getppid\n");
+    fflush(stdout);
     for( j = 0; i < 100000; ++i){
     }
     fflush(stdout);
 
     for( j = 0; i < 10000000; ++i){
     }
+    Sleep(5000);
   }
 
 
